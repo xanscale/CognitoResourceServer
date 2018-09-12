3ea0243c35b09ae530a6b8a0ea4e0689575628a9
Alessandro Scarozza <xan.scale@gmail.com>
2018-09-12 12:02:58 +0200

    fix

--- src/main/java/it/localhostsoftware/security/oauth2/provider/token/store/CognitoAccessTokenConverter.java
+++ src/main/java/it/localhostsoftware/security/oauth2/provider/token/store/CognitoAccessTokenConverter.java
@@ -8,19 +8,17 @@ import java.util.Map;
 
 @Component
 public class CognitoAccessTokenConverter extends JwtAccessTokenConverter {
+	private static final String COGNITO_GROUPS = "cognito:groups";
+	private static final String AUTHORITIES = "authorities";
+	private static final String USERNAME = "username";
+	private static final String USER_NAME = "user_name";
+
 	@Override
 	public OAuth2Authentication extractAuthentication(Map<String, ?> claims) {
-	/*	ArrayList<String> groupList = ((ArrayList<String>) claims.get("cognito:groups"));
-		if (groupList == null)
-			groupList = new ArrayList<>();
-		String[] groups = groupList.toArray(new String[0]);
-		OAuth2Authentication authentication = super.extractAuthentication(claims);
-		OAuth2Request old = authentication.getOAuth2Request();
-		OAuth2Request newRequest = new OAuth2Request(old.getRequestParameters(), old.getClientId(), AuthorityUtils.createAuthorityList(groups),
-				true, old.getScope(), old.getResourceIds(), null, null, null);
-		return new OAuth2Authentication(newRequest, authentication.getUserAuthentication());*/
-		((Map<String, Object>) claims).put("authorities", claims.get("cognito:groups"));
-		((Map<String, Object>) claims).put("user_name", claims.get("username"));
+		if (claims.containsKey(COGNITO_GROUPS))
+			((Map<String, Object>) claims).put(AUTHORITIES, claims.get(COGNITO_GROUPS));
+		if (claims.containsKey(USERNAME))
+			((Map<String, Object>) claims).put(USER_NAME, claims.get(USERNAME));
 		return super.extractAuthentication(claims);
 	}
 }
